a207eb18926c7eab30b30142d7dfecf363b36e0a
Sinarck <83467783+sinarck@users.noreply.github.com>
2024-01-03 16:15:27 -0600

    feat: successful reverse engineering of xello's internal api

--- src/api/xello.ts
+++ src/api/xello.ts
@@ -1,41 +1,101 @@
-import axios from "axios";
+import axios, { AxiosError, AxiosResponse } from "axios";
+import dotenv from "dotenv";
 
-const login = async () => {
+// Load environment variables
+dotenv.config();
+
+interface XelloResponse {
+  data: Login;
+}
+
+export interface Login {
+  url: string;
+  isMustResetPassword: boolean;
+  userAccountId: number;
+  jwtToken: string;
+  userType: number;
+  grade: number;
+  isSource: boolean;
+}
+
+interface Experiences {
+  id: number;
+  experienceCategoryId: string;
+  experience: string;
+  organization: string;
+  city: string;
+  stateProvince: string;
+  country: string;
+  formattedAddress: string;
+  startDate: string;
+  endDate: string;
+  isOngoing: boolean;
+  liked: string;
+  disliked: string;
+  learned: string;
+  serviceHour: number;
+  showInResume: boolean;
+  resumeNotes: any;
+}
+
+const getToken = async (username: string, password: string) => {
+  // Payload reverse engineered from xello login page
   const payload = {
-    Username: process.env.EXPO_PUBLIC_XELLO_USERNAME,
-    Password: process.env.EXPO_PUBLIC_XELLO_PASSWORD,
+    Username: username,
+    Password: password,
     SelectedLanguage: "en-US",
     remember: true,
   };
 
-  await axios
-    .post("https://login.xello.world/api/auth/login", payload)
-    .then((res) => {
-      console.info(res.data.data);
-      return res.data.data;
+  // Make the POST request to their servers
+  return await axios
+    .post<XelloResponse>("https://login.xello.world/api/auth/login", payload)
+    .then((res: AxiosResponse<XelloResponse, any>) => {
+      // Return user object containing jwt token
+      return res.data.data.jwtToken;
     })
-    .catch((err) => {
-      console.error(err);
+    .catch((err: AxiosError) => {
+      // Print the 'data' field in the response
+      console.error(err.response.data);
     });
 };
-const getExperiences = async () => {
-  const authorization = await login();
-  console.log("Authorization jwt token:", authorization);
 
-  const experienceScreenResponse = await axios
+const experiences = async (jwtToken: XelloResponse["data"]["jwtToken"]) => {
+  if (!jwtToken) throw new Error("No JWT token provided");
+
+  await axios
     .get("https://student.xello.world/api/experiences", {
       headers: {
         Culture: "en-US",
         Dnt: "1",
-        Authorization: +authorization,
+        Authorization: jwtToken,
       },
     })
-    .then((res) => {
-      console.log(res.data);
+    .then((res: AxiosResponse<Experiences[], any>) => {
+      console.log("Succesfully retrieved experiences");
+      res.data.map((experience) => {
+        console.table(experience);
+      });
     })
-    .catch((err) => {
+    .catch((err: AxiosError) => {
       console.error(err);
     });
 };
 
-login();
+const getExperiences = async () => {
+  await getToken(
+    process.env.EXPO_PUBLIC_XELLO_USERNAME,
+    process.env.EXPO_PUBLIC_XELLO_PASSWORD
+  )
+    .then((res) => {
+      if (!res) throw new Error("No JWT token provided");
+
+      experiences(res);
+    })
+    .catch((err) => {
+      console.error("Login failed");
+      console.error(err);
+    });
+};
+
+getExperiences();
