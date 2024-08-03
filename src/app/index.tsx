5a604e3bcff3aaeb30faed5020d4d259369e98f0
Aadi <83467783+sinarck@users.noreply.github.com>
2024-08-03 17:50:45 -0500

    feat: basic placeholder `ErrorBoundary` wrapping the application

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,14 @@
-import 'expo-dev-client';
 import { View } from 'react-native';
 import MonthlyGoal from '../components/home/monthly-goal';
 
 const App = () => {
+  throw new Error("Test Error Boundary")
+
   return (
     <View className="pt-offset pb-offset">
-      <View className="flex min-h-screen m-page">
+      <View className="flex min-h-screen m-page space-y-4">
         <MonthlyGoal hoursLeft={2} monthlyGoal={10} />
+
       </View>
     </View>
   );
---
+++ src/components/fallback.tsx
@@ -0,0 +1,20 @@
+import { Feather } from '@expo/vector-icons';
+import { Text, View } from 'react-native';
+import palette from '../lib/palette';
+
+const Fallback = () => {
+  return (
+    <View className="flex flex-col justify-center items-center align-middle min-h-screen m-page space-y-1">
+      <Feather
+        name="alert-triangle"
+        size={128}
+        color={palette['neutral']['500']}
+      />
+      <Text className="text-neutral-500 font-popMedium text-2xl">
+        Something went wrong
+      </Text>
+    </View>
+  );
+};
+
+export default Fallback;
