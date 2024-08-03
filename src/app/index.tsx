181f0b69c6b8a124ad2670b6401ddbb4cd3ab69c
Aadi <83467783+sinarck@users.noreply.github.com>
2024-08-03 17:51:01 -0500

    fix: remove testing error

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,13 +2,10 @@ import { View } from 'react-native';
 import MonthlyGoal from '../components/home/monthly-goal';
 
 const App = () => {
-  throw new Error("Test Error Boundary")
-
   return (
     <View className="pt-offset pb-offset">
       <View className="flex min-h-screen m-page space-y-4">
         <MonthlyGoal hoursLeft={2} monthlyGoal={10} />
-
       </View>
     </View>
   );
